f2113fabbc482e36647cdadce8fae6bfa172b4ef
rio <caecarioyonim@gmail.com>
2024-04-19 14:08:17 +0700

    Storeusers

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LetterController;
-use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\API\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,5 @@ Route::get('users', [UserController::class, 'index']);
 Route::get('users/{id}', [UserController::class, 'show']);
 Route::put('users/{id}', [UserController::class, 'update']);
 Route::delete('users/{id}', [UserController::class, 'delete']);
+Route::post('/storeusers', [UserController::class, 'storeUser']);
+
